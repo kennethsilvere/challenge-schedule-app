6dd2bc44c8256e249fccac5205cec202504dc34f
Kenneth Silvere <kennethsilvere93@gmail.com>
2021-08-19 12:24:04 +0530

    Add styles to differentiate completed & cancelled appointments from others

--- src/components/Appointment/Appointment.tsx
+++ src/components/Appointment/Appointment.tsx
@@ -23,51 +23,58 @@ const Appointment: FunctionComponent<Props> = (props: Props) => {
     appointmentCtx.setAllAppointments(updatedAppointments)
   }
   return (
-    <div className='appointment-container'>
-      <div className='appointment-detail'>
-        <p>
-          Name: <span className='data-value'>{props.data.patientName}</span>
-        </p>
-        <img
-          src={props.data.patientImage}
-          alt={props.data.patientName}
-          height='100px'
-        />
-      </div>
-      <div className='appointment-detail'>
-        <p>
-          Date: <span className='data-value'>{props.data.requestedDate}</span>
-        </p>
-        <p>
-          Reason: <p className='data-value'>{props.data.requestReason}</p>
-        </p>
-        {(props.data.status === "new" || props.data.status === "confirmed") && (
+    <div className='appointment-element'>
+      {(props.data.status === "completed" ||
+        props.data.status === "cancelled") && <div className='overlay'></div>}
+      <div className='appointment-container'>
+        <div className='appointment-detail'>
           <p>
-            <span>Actions:</span>
-            <button onClick={cancelApppointment} className='cancel-btn'>
-              Cancel
-            </button>
+            Name: <span className='data-value'>{props.data.patientName}</span>
           </p>
-        )}
-        {props.data.cancellationReason && (
-          <p>
-            Reason for Cancellation:{" "}
-            <span className='data-value'>{props.data.cancellationReason}</span>
-          </p>
-        )}
-      </div>
-      <div className='appointment-detail'>
-        <p>
-          Doctor:{" "}
-          <span className='data-value'>{props.data.assignedDoctorName}</span>
-        </p>
-        {props.data.assignedDoctorImage && (
           <img
-            src={props.data.assignedDoctorImage}
-            alt={props.data.assignedDoctorName}
+            src={props.data.patientImage}
+            alt={props.data.patientName}
             height='100px'
           />
-        )}
+        </div>
+        <div className='appointment-detail'>
+          <p>
+            Date: <span className='data-value'>{props.data.requestedDate}</span>
+          </p>
+          <p>
+            Reason: <p className='data-value'>{props.data.requestReason}</p>
+          </p>
+          {(props.data.status === "new" ||
+            props.data.status === "confirmed") && (
+            <p>
+              <span>Actions:</span>
+              <button onClick={cancelApppointment} className='cancel-btn'>
+                Cancel
+              </button>
+            </p>
+          )}
+          {props.data.cancellationReason && (
+            <p>
+              Reason for Cancellation:{" "}
+              <span className='data-value'>
+                {props.data.cancellationReason}
+              </span>
+            </p>
+          )}
+        </div>
+        <div className='appointment-detail'>
+          <p>
+            Doctor:{" "}
+            <span className='data-value'>{props.data.assignedDoctorName}</span>
+          </p>
+          {props.data.assignedDoctorImage && (
+            <img
+              src={props.data.assignedDoctorImage}
+              alt={props.data.assignedDoctorName}
+              height='100px'
+            />
+          )}
+        </div>
       </div>
     </div>
   )
